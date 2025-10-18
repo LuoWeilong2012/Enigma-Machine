f748daa456d512593dad015cee770a8ff184a552
罗伟龙 <youkebale@qq.com>
2025-10-18 21:18:49 +0800

    Delete EnigmaMachine directory

--- EnigmaMachine/logicsection/path.py
+++
@@ -1,5 +0,0 @@
-import sys
-sysx=sys.path[0]
-sys.path.append(sysx+"\\rotor")
-sys.path.append(sysx+"\\reflector")
-sys.path.append(sysx+"\\plugboard")
--- EnigmaMachine/reflector/reflector.py
+++
@@ -1,8 +0,0 @@
-def out(a,b):
-    result = ''.join([char for char in a if char not in b])        
-    return result
-def reflector():
-    word="qwertyuiopasdfghjklzxcvbnm"
-    one="mnbvcxzlkjhgf"
-    outto=out(word,one)
-
